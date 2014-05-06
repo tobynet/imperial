348391378bb34d6b514aadc3681996cc38c272b2
toooooooby <toby.net.info.mail+git@gmail.com>
2014-05-06 18:46:26 +0900

    Refactor before modulize

--- imperial.ps1
+++ imperial.ps1
@@ -6,20 +6,52 @@ Param (
     [string]$csvFile
 )
 
-function renderTemplate($source, $variables) {
+
+<#
+e.g.
+    
+    > Render-Template -source "{{foo}} bar" -variables @{foo="REPLACED"}
+    REPLACED bar
+    
+#>
+function Render-Template([string]$source, [Hashtable]$variables) {
     [regex]::Replace( $source, "{{([\w_]+)}}", {
             param($matched)
             $variables[$matched.Groups[1].Value]
         })
 }
 
-function getHashTableFromCSVFile($csvFile) {
+<#
+e.g.
+    > Get-Content foo.csv
+    foo,123
+    bar,456
+
+    > Get-HashTableFromCSV (Import-Csv "foo.csv")
+    Name           Value
+    ----           -----
+    foo            123
+    bar            456
+#>
+function Get-HashTableFromCSV([Object[]]$csv) {
     $variables = @{}
-    Import-Csv $csvFile | %{ $variables[$_.key] = $_.value }
+    $csv | %{ $variables[$_.key] = $_.value }
     $variables
 }
 
-function renderExcel($inputFilename, $outputFilename, $variables) {
+
+<#
+e.g.1
+
+    > Render-Excel -inputFilename "template.xsl" -outputFilename "out.xsl" -variables @{foo="bar",buzz=1234}
+
+e.g.2
+
+    > $hash = Get-HashTableFromCSV (Import-Csv "foo.csv")
+    > Render-Excel -inputFilename "template.xsl" -outputFilename "out.xsl" -variables $hash
+    
+#>
+function Render-Excel([string]$inputFilename, [string]$outputFilename, [HashTable]$variables) {
     echo ( "Rendering an excel file from {0} to {1}" -f $inputFilename , $outputFilename )
     
     # Making an Excel object
@@ -44,12 +76,12 @@ function renderExcel($inputFilename, $outputFilename, $variables) {
             
             if ($range.Rows.Count -eq 1) {
                 # It is a NOT 2-dimensions array if count of Rows equls 1 !?!?!?????
-                $buffer = renderTemplate $buffer $variables
+                $buffer = Render-Template $buffer $variables
             } else {
                 1..$range.Rows.Count | %{ $y = $_
                     1..$range.Columns.Count | %{ $x = $_
                         #echo ("  [{0},{1}] : {2}" -f $y, $x, $buffer[$y, $x])
-                        $buffer[$y, $x] = renderTemplate $buffer[$y, $x] $variables
+                        $buffer[$y, $x] = Render-Template $buffer[$y, $x] $variables
                     }
                 }
             }
@@ -57,13 +89,15 @@ function renderExcel($inputFilename, $outputFilename, $variables) {
             
             $range.Value2 = $buffer
         }
-        echo ( "Save as {0}" -f $outputFilename )
+        echo ( "Save as $outputFilename")
         $books.SaveAs($outputFilename)
     } finally {
         $excel.Quit()
     }
 }
 
-renderExcel $(ls $inputFile).fullname $([Io.Path]::GetFullPath($outputFile)) $(getHashTableFromCSVFile $csvFile)
-
+Render-Excel `
+    -inputFilename (ls $inputFile).fullname `
+    -outputFilename ([Io.Path]::GetFullPath($outputFile)) `
+    -variables (Get-HashTableFromCSV (Import-Csv $csvFile))
 echo "DONE"
